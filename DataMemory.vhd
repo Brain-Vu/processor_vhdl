a6fc507b1560f2629fbe200c22393b3fa67acb46
Win-Peerapong <peerapongputtachaipanit@gmail.com>
2026-06-05 15:34:40 -0700

    Update DataMemory.vhd

--- DataMemory.vhd
+++ DataMemory.vhd
@@ -3,31 +3,25 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 USE ieee.std_logic_unsigned.all;
 
---------------------------------------------------------------------------------
--- RAM MODULE
---------------------------------------------------------------------------------
-
 entity DataMemory is
     Port(
-        Reset   : in std_logic;
-        Clock   : in std_logic;
-        OE      : in std_logic;
-        WE      : in std_logic;
-        Address : in std_logic_vector(29 downto 0);
-        DataIn  : in std_logic_vector(31 downto 0);
+        Reset   : in  std_logic;
+        Clock   : in  std_logic;
+        OE      : in  std_logic;
+        WE      : in  std_logic;
+        Address : in  std_logic_vector(29 downto 0);
+        DataIn  : in  std_logic_vector(31 downto 0);
         DataOut : out std_logic_vector(31 downto 0)
     );
 end entity DataMemory;
 
 architecture staticRAM of DataMemory is
-
     type ram_type is array (0 to 127) of std_logic_vector(31 downto 0);
     signal i_ram : ram_type;
     signal highz : std_logic_vector(31 downto 0) := "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
-
 begin
 
-    -- WRITE AND RESET PROCESS
+    -- write and reset process
     RamWrite : process(Clock, Reset)
     begin
         if Reset = '1' then
@@ -43,7 +37,7 @@ begin
         end if;
     end process RamWrite;
 
-    -- READ PROCESS
+    -- read process
     RamRead : process(OE, Address, i_ram)
     begin
         if (OE = '0' AND (to_integer(unsigned(Address)) <= 127)) then
