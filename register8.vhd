e72f229ab9097596ac507754668d3a5e863221b5
Win-Peerapong <peerapongputtachaipanit@gmail.com>
2026-06-05 10:09:46 -0700

    Create register8.vhd

---
+++ register8.vhd
@@ -0,0 +1,31 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity register8 is
+port(
+    datain  : in  std_logic_vector(7 downto 0);
+    enout   : in  std_logic;
+    writein : in  std_logic;
+    dataout : out std_logic_vector(7 downto 0)
+);
+end entity register8;
+
+architecture memmy of register8 is
+component bitstorage
+port(
+    bitin   : in  std_logic;
+    enout   : in  std_logic;
+    writein : in  std_logic;
+    bitout  : out std_logic
+);
+end component;
+begin
+    m0: bitstorage port map(datain(0), enout, writein, dataout(0));
+    m1: bitstorage port map(datain(1), enout, writein, dataout(1));
+    m2: bitstorage port map(datain(2), enout, writein, dataout(2));
+    m3: bitstorage port map(datain(3), enout, writein, dataout(3));
+    m4: bitstorage port map(datain(4), enout, writein, dataout(4));
+    m5: bitstorage port map(datain(5), enout, writein, dataout(5));
+    m6: bitstorage port map(datain(6), enout, writein, dataout(6));
+    m7: bitstorage port map(datain(7), enout, writein, dataout(7));
+end architecture memmy;
